1f82e9520edd2f2b33ab04bac8f80bb7b43d9e30
Yassir aittata <yassir.aittata@gmail.com>
2025-08-27 15:57:12 +0100

    add hero section

---
+++ client/src/components/Hero.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Hero = () => {
+  return (
+    <div className="px-4 sm:px-20 xl:px-32 relative inline-flex flex-col w-full justify-center bg-[url(/gradientBackground.png)] bg-cover bg-no-repeat min-h-screen">
+      Hero
+    </div>
+  );
+};
+
+export default Hero;
