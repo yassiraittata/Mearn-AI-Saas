03399f66b78b6131b0d5d767d6af7753b8e98239
Yassir aittata <yassir.aittata@gmail.com>
2025-08-27 16:07:33 +0100

    add CTA buttons

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -1,6 +1,9 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const Hero = () => {
+  const navigate = useNavigate();
+
   return (
     <div className="px-4 sm:px-20 xl:px-32 relative inline-flex flex-col w-full justify-center bg-[url(/gradientBackground.png)] bg-cover bg-no-repeat min-h-screen">
       <div className="text-center mb-6">
@@ -12,6 +15,18 @@ const Hero = () => {
           Leverage the power of AI to write articles, generate images, create
           blog titles and more.
         </p>
+      </div>
+
+      <div className="flex flex-wrap justify-center gap-4 text-sm max-sm:text-xs">
+        <button
+          onClick={() => navigate("/ai")}
+          className="bg-primary text-white px-10 py-3 rounded-lg hover:scale-105 active:scale-95 transition cursor-pointer"
+        >
+          Start creating now
+        </button>
+        <button className="bg-white px-10 py-3 rounded-lg border border-gray-300 hover:scale-105 active:scale-95 transition cursor-pointer">
+          Watch demo
+        </button>
       </div>
     </div>
   );
