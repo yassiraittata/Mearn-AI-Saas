ac9220baf97a75f802a2966d0ad58cab326067c8
Yassir aittata <yassir.aittata@gmail.com>
2025-09-01 16:09:06 +0100

    add nav

--- client/src/pages/Layout.jsx
+++ client/src/pages/Layout.jsx
@@ -1,10 +1,28 @@
-import React from "react";
-import { Outlet } from "react-router-dom";
+import React, { useState } from "react";
+import { Outlet, useNavigate } from "react-router-dom";
+import { assets } from "../assets/assets";
+import { Menu, X } from "lucide-react";
 
 const Layout = () => {
+  const navigate = useNavigate();
+  const [sidebar, setSidebar] = useState(false);
+
   return (
-    <div>
-      <h1>Layout</h1>
+    <div className="flex flex-col items-start justify-start h-screen">
+      <nav className="w-full px-8 min-h-14 flex items-center justify-between border-b border-gray-200">
+        <img src={assets.logo} alt="Logo" onClick={() => navigate("/")} />
+        {sidebar ? (
+          <X
+            className="w-6 h-6 text-gray-600 sm:hidden"
+            onClick={() => setSidebar(false)}
+          />
+        ) : (
+          <Menu
+            className="w-6 h-6 text-gray-600 sm:hidden"
+            onClick={() => setSidebar(true)}
+          />
+        )}
+      </nav>
       <Outlet />
     </div>
   );
