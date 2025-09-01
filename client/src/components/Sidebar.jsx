62b84f19b48cfc2f872b531250cca6402a63c640
Yassir aittata <yassir.aittata@gmail.com>
2025-09-01 17:11:30 +0100

    add links into sidebar

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -1,4 +1,58 @@
 import { useClerk, useUser } from "@clerk/clerk-react";
+import {
+  Eraser,
+  FileText,
+  Hash,
+  House,
+  Image,
+  Scissors,
+  SquarePen,
+  Users,
+} from "lucide-react";
+import { NavLink } from "react-router-dom";
+
+const navItems = [
+  {
+    to: "/ai",
+    Label: "Dashboard",
+    Icon: House,
+  },
+  {
+    to: "/ai/write-article",
+    Label: "Write Article",
+    Icon: SquarePen,
+  },
+  {
+    to: "/ai/blog-titles",
+    Label: "Blog Titles",
+    Icon: Hash,
+  },
+  {
+    to: "/ai/generate-images",
+    Label: "Generate Images",
+    Icon: Image,
+  },
+  {
+    to: "/ai/remove-background",
+    Label: "Remove Background",
+    Icon: Eraser,
+  },
+  {
+    to: "/ai/remove-object",
+    Label: "Remove Object",
+    Icon: Scissors,
+  },
+  {
+    to: "/ai/review-resume",
+    Label: "Review Resume",
+    Icon: FileText,
+  },
+  {
+    to: "/ai/community",
+    Label: "Community",
+    Icon: Users,
+  },
+];
 
 const Sidebar = ({ sidebar, setSidebar }) => {
   const { user } = useUser();
@@ -16,6 +70,30 @@ const Sidebar = ({ sidebar, setSidebar }) => {
           className="w-14 rounded-full mx-auto"
         />
         <h1 className="mt-1 text-center">{user.fullName}</h1>
+        <div className="px-4 mt-5 text-sm text-gray-600 font-medium">
+          {navItems.map(({ Icon, Label, to }) => (
+            <NavLink
+              key={to}
+              to={to}
+              end={to === "/ai"}
+              onClick={() => setSidebar(false)}
+              className={({ isActive }) =>
+                `px-3.5 py-2.5 flex items-center gap-3 rounded ${
+                  isActive
+                    ? "bg-gradient-to-r from-[#3C81F6] to-[#9234EA] text-white"
+                    : ""
+                }`
+              }
+            >
+              {({ isActive }) => (
+                <>
+                  <Icon className={`w-4 h-4 ${isActive ? "text-white" : ""}`} />{" "}
+                  {Label}
+                </>
+              )}
+            </NavLink>
+          ))}
+        </div>
       </div>
     </div>
   );
