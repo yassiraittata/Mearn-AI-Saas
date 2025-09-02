0d85e1b5e1cf7e363884ffd9ee137d6e464c7c36
Yassir aittata <yassir.aittata@gmail.com>
2025-09-02 16:42:18 +0100

    enhance css

--- client/src/components/CreationItem.jsx
+++ client/src/components/CreationItem.jsx
@@ -32,7 +32,7 @@ const CreationItem = ({ item }) => {
             </div>
           ) : (
             <div className="mt-3 h-full overflow-y-scroll text-sm text-slate-700">
-              <div>
+              <div className="reset-tw">
                 <Markdown>{item.content}</Markdown>
               </div>
             </div>
