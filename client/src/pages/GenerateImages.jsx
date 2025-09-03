8ee73a98cf52576f0200de9f4108871c18c7f2fc
Yassir aittata <yassir.aittata@gmail.com>
2025-09-03 21:26:19 +0100

    generate image page

--- client/src/pages/GenerateImages.jsx
+++ client/src/pages/GenerateImages.jsx
@@ -1,7 +1,76 @@
-import React from "react";
+import { Image, Sparkles } from "lucide-react";
+import React, { useState } from "react";
 
 const GenerateImages = () => {
-  return <div>GenerateImages</div>;
+  const imageStyle = ["Realistic", "Ghibli"];
+
+  const [selectedImageStyle, setSelectedImageStyle] = useState(imageStyle[0]);
+  const [input, setInput] = useState("");
+  const [publish, setPublish] = useState(false);
+
+  const submithandler = async (e) => {
+    e.preventDefault();
+    // handle form submission
+  };
+
+  return (
+    <div className="h-full overflow-y-scroll p-6 flex items-start flex-wrap gap-4 text-slate-700">
+      {/* left col */}
+      <form
+        onSubmit={submithandler}
+        className="w-full max-w-lg p-4 bg-white rounded-lg border border-gray-200"
+      >
+        <div className="flex items-center gap-3">
+          <Sparkles className="w-6 text-[#00AD25]" />
+          <h1 className="text-xl font-semibold">AI Image Generator</h1>
+        </div>
+        <p className="mt-6 text-sm font-medium">Describe Your Image</p>
+        <textarea
+          type="text"
+          className="w-full p-2 px-3 mt-2 outline-none text-sm rounded-md border border-gray-300"
+          placeholder="Describe what you want to see in the image..."
+          required
+          rows={5}
+          onChange={(e) => setInput(e.target.value)}
+          value={input}
+        ></textarea>
+        <p className="mt-4 text-sm font-medium">Style</p>
+        <div className="mt-3 flex gap-3 flex-wrap sm:max-w-9/11">
+          {imageStyle.map((item, index) => (
+            <span
+              key={index}
+              className={`text-xs px-4 py-1 border rounded-full cursor-pointer ${
+                selectedImageStyle === item
+                  ? "bg-purple-50 text-purple-700"
+                  : "text-gray-500 border-gray-300"
+              }`}
+              onClick={() => setSelectedImageStyle(item)}
+            >
+              {item}
+            </span>
+          ))}
+        </div>
+        <br />
+        <button className="w-full flex justify-center items-center gap-2 bg-gradient-to-r from-[#00AD25] to-[#04FF50] text-white px-4 py-2 mt-6 text-sm rounded-lg cursor-pointer">
+          <Image className="w-5" /> Generate Image
+        </button>
+      </form>
+      {/* right col */}
+      <div className="w-full max-w-lg p-4 bg-white rounded-lg flex flex-col border border-gray-200 min-h-96">
+        <div className="flex items-center gap-3">
+          <Image className="w-5 text-[#00AD25]" />
+          <h1 className="text-xl font-semibold">Generated image</h1>
+        </div>
+
+        <div className="flex-1 flex items-center justify-center">
+          <div className="text-sm flex flex-col items-center gap-5 text-gray-400">
+            <Image className="w-9 h-9" />
+            <p>Describe an image and click "Generate image" to get started </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default GenerateImages;
