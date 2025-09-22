54620a180197a11ad91e1aa7ef7d82f53b1b5826
Yassir aittata <yassir.aittata@gmail.com>
2025-09-22 16:29:50 +0100

    get all user creations

---
+++ server/controllers/userController.js
@@ -0,0 +1,12 @@
+import createError from "http-errors";
+import { clerkClient, getAuth } from "@clerk/express";
+import sql from "../configs/db.js";
+
+export const getUserCeations = async (req, res) => {
+  const { userId } = getAuth(req);
+
+  const creations =
+    await sql`SELECT * FROM creations WHERE user_id = ${userId} ORDER BY created_at DESC`;
+
+  res.json({ success: true, creations });
+};
