d9f87fbf70950d7326825d2da9e55b9049da1b71
Yassir aittata <yassir.aittata@gmail.com>
2025-09-22 16:42:19 +0100

    toggle likes in a creation

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -12,10 +12,38 @@ export const getUserCeations = async (req, res) => {
 };
 
 export const getPublishedCeations = async (req, res) => {
-  const { userId } = getAuth(req);
-
   const creations =
     await sql`SELECT * FROM creations WHERE pubish = true ORDER BY created_at DESC`;
 
   res.json({ success: true, creations });
+};
+
+export const toggleLikeCreations = async (req, res) => {
+  const { userId } = getAuth(req);
+  const { id } = req.body;
+
+  const [creations] = await sql`SELECT * FROM creations WHERE id = ${id}`;
+
+  if (!creations) {
+    throw createError(404, "Creation not found");
+  }
+
+  let currentLikes = creations.likes || [];
+  const userIdStr = userId.toString();
+  let updatedLikes;
+  let message;
+
+  if (currentLikes.includes(userIdStr)) {
+    updatedLikes = currentLikes.filter((user) => user !== userIdStr);
+    message = "Creation unliked";
+  } else {
+    updatedLikes = [...currentLikes, userIdStr];
+    message = "Creation liked";
+  }
+
+  const formattedArray = `{${updatedLikes.join(",")}}`;
+
+  await sql`UPDATE creations SET likes = ${formattedArray}::text[] WHERE id = ${id}`;
+
+  res.json({ success: true, message });
 };
