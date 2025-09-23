449ca40c2772bccdcb1d3b79187c8a8f5dda39db
Yassir aittata <yassir.aittata@gmail.com>
2025-09-23 16:34:04 +0100

    display generatd content

--- client/src/pages/WriteArticle.jsx
+++ client/src/pages/WriteArticle.jsx
@@ -97,8 +97,17 @@ const WriteArticle = () => {
           ))}
         </div>
         <br />
-        <button className="w-full flex justify-center items-center gap-2 bg-gradient-to-r from-[#226BFF] to-[#65ADFF] text-white px-4 py-2 mt-6 text-sm rounded-lg cursor-pointer">
-          <Edit className="w-5" /> Generate Article
+        <button
+          disabled={loading}
+          className="w-full flex justify-center items-center gap-2 bg-gradient-to-r from-[#226BFF] to-[#65ADFF] text-white px-4 py-2 mt-6 text-sm rounded-lg cursor-pointer"
+        >
+          {" "}
+          {loading ? (
+            <span className="w-4 h-4 my-1 rounded-full border-2 border-t-transparent animate-spin"></span>
+          ) : (
+            <Edit className="w-5" />
+          )}
+          Generate Article
         </button>
       </form>
       {/* right col */}
@@ -108,12 +117,20 @@ const WriteArticle = () => {
           <h1 className="text-xl font-semibold">Generated Article</h1>
         </div>
 
-        <div className="flex-1 flex items-center justify-center">
-          <div className="text-sm flex flex-col items-center gap-5 text-gray-400">
-            <Edit className="w-9 h-9" />
-            <p>Enter a topic and click "Generate article" to get started </p>
+        {!content ? (
+          <div className="flex-1 flex items-center justify-center">
+            <div className="text-sm flex flex-col items-center gap-5 text-gray-400">
+              <Edit className="w-9 h-9" />
+              <p>Enter a topic and click "Generate article" to get started </p>
+            </div>
           </div>
-        </div>
+        ) : (
+          <>
+            <div className="mt-3 h-full overflow-y-scroll text-sm text-slate-600">
+              <div>{content}</div>
+            </div>
+          </>
+        )}
       </div>
     </div>
   );
